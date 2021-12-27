e33afaa693bf00f7be9ddeeefda935bfb9e5a06d
TomLloyd92 <c00231308@itcarlow.ie>
2021-12-27 23:36:39 +0000

    Speach bubble facing camera

---
+++ Assets/Scripts/Multiplayer/Messenger/FaceCamera.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private Transform mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotate to player
+        //transform.LookAt(transform.position + mainCamera.rotation * Vector3.forward,
+        //    mainCamera.rotation * Vector3.up);
+
+
+        Quaternion r1 = Quaternion.LookRotation(transform.position - mainCamera.transform.position, Vector3.up);
+
+        Vector3 euler2 = transform.eulerAngles;
+
+        //euler2.x
+        // euler2.z
+        transform.rotation = Quaternion.Euler(0, r1.eulerAngles.y, 0);
+
+
+    }
+}
--- Assets/Scripts/Multiplayer/Messenger/MessageBubble.cs
+++ Assets/Scripts/Multiplayer/Messenger/MessageBubble.cs
@@ -5,14 +5,32 @@ using TMPro;
 
 public class MessageBubble : MonoBehaviour
 {
+    [SerializeField]
     private SpriteRenderer bubbleSprite;
-    private TextMeshPro text;
+    [SerializeField]
+    private TMP_Text textTMP;
 
 
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    private void Awake()
+    {
+        setUp("hello");
+    }
+
+    private void setUp(string text)
+    {
+        textTMP.SetText(text);
+        //Make sure it updates
+        textTMP.ForceMeshUpdate();
+        Vector2 textsize = new Vector2 (textTMP.GetRenderedValues(false).x + 50 , textTMP.GetRenderedValues(false).y + 50);
+        Vector2 padding = new Vector2(50f, 50f);
+        bubbleSprite.size = textsize + padding; 
+
     }
 
     // Update is called once per frame
